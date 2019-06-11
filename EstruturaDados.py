4028d6a3f9fa5b59956d1c214e7669b041c228b4
jadsonmarliere <jds.mdo@gmail.com>
2019-06-11 17:34:02 -0300

    indices

--- EstruturaDados.py
+++ EstruturaDados.py
@@ -13,7 +13,17 @@ del(d['nome'])
 print(d)
 
 t = (1,1,2,3,5,8,13,21)
-print(t.index(1))
+print(t.index(5))
+print(t[4])
+print(t[t.index(5)])
+
+def veficicaIndice():
+    if t[4] == t[t.index(5)]:
+        return True
+    else:
+        return False
+print(veficicaIndice())
+
 print(t)
 
 s ={'Jadson', 'Keina', 'Katy'}
