4e8ffacdeeee6b690c24ac4d27132f929e579761
jadsonmarliere <jds.mdo@gmail.com>
2019-06-26 13:48:38 -0300

    Uso de ' with as'

--- arquivos.py
+++ arquivos.py
@@ -1,14 +1,32 @@
 arq = open('arq_demo.txt', mode='w') # sobrescreve
-arq.write('salvando string...')
+arq.write('salvando string...\n')
 arq.close()
 
 arq = open('arq_demo.txt', mode='a')
-arq.write('adicionando conteudo ...')
+arq.write('adicionando conteudo ...\n')
 arq.close()
 
 arq = open('arq_demo.txt', mode='r')
 conteudo_arquivo=arq.read()
 arq.close()
 
-print(conteudo_arquivo)
+# para ler varias linhas
+'''linha = arq.readline()
+while linha:
+    print(linha)
+    linha = arq.readline()
+arq.close()
+print(conteudo_arquivo)'''
+
+# uso de with
+with open('arq_demo.txt', mode='w') as arq:
+    '''
+    linha = arq.readline()
+    while linha:
+        print(linha)
+        linha = arq.readline() '''
+
+    arq.write('Testa se esta fechando !')
+entrada_usuario = input('Digite algo: ')
+print('Fim.')
 
