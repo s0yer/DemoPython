19d5f791189058c0ea4a4a6b987b722646777398
jadsonmarliere <jds.mdo@gmail.com>
2019-10-10 12:07:49 -0300

    update
    plusMinus

--- Funcoes.py
+++ Funcoes.py
@@ -1,4 +1,5 @@
 
+
 #tupla
 def ArgumentosInfinitos(*args):
     print(args)
@@ -97,7 +98,31 @@ def compareTriplets(a, b):
 n = 10
 arr = [n]
 
+def plusMinus(arr):
+    neg = 0
+    pos = 0
+    zeros = 0
 
+    for el in arr:
+        if -100 <= el <= 100:
+            if el < 0:
+                neg += 1
+            elif el > 0:
+                pos += 1
+            else:
+                zeros += 1
+        else:
+            print('You need to introduce a number between -100 and 100!')
+
+    ratioNeg = neg / n
+    ratioPos = pos / n
+    ratioZeros = zeros / n
+
+    print('{:f}'.format(ratioPos))
+    print('{:f}'.format(ratioNeg))
+    print('{:f}'.format(ratioZeros))
+
+    return ratioPos, ratioNeg, ratioZeros
 
 
 
