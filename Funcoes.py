30a5257b434df1f9b5ccb408439bc5f708213fe2
jadsonmarliere <jds.mdo@gmail.com>
2019-10-19 19:18:59 -0300

    update Funcoes

--- Funcoes.py
+++ Funcoes.py
@@ -407,3 +407,30 @@ def solve(meal_cost, tip_percent, tax_percent):
     return int(round(total_cost))
 
 #------------------------------------------------------------------------------------
+'''
+n = int(input())
+arr = map(int, input().split())
+'''
+# Runner up score
+
+n = 5
+arr = [3,1,6,6,5]
+def runnerUp(n,arr):
+    if 2 <= n <= 10:
+        arr_sorted = sorted(arr)
+        last_higher = max(arr_sorted)
+        i = 0
+
+        while i < n:
+            if arr[-(i+1)] == last_higher:
+                i += 1
+            else:
+                runner_up = arr[-(i+1)]
+                i = n
+
+        print(runner_up)
+
+    else:
+        print('imput a value between [2,10]')
+
+runnerUp(n,arr)
