f65463ba3945f07179e32732017cc2e97ffc70df
jadsonmarliere <jds.mdo@gmail.com>
2019-10-18 21:15:16 -0300

    Update 30days.py

--- 30days.py
+++ 30days.py
@@ -13,3 +13,22 @@ print(s+ss)
 
 #----------------------------------------------------------------------
 
+#odd and even
+N = 77
+def oddEven(N):
+    if 0 <= N <= 100:
+        if N % 2 == 0:
+            if 2 <= N <= 5:
+                print('Not Weird')
+            elif 6 <= N <= 20:
+                print('Weird')
+            else:
+                print('Not Weird')
+        else:
+            print('Weird')
+    else:
+        print('imput a valid number!')
+
+oddEven(N)
+
+#----------------------------------------------------------------------
