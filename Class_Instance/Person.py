1641e8632e2e11c09e55787521fdbd71fefb035c
jadsonmarliere <jds.mdo@gmail.com>
2019-10-28 20:05:37 -0300

    Update Person.py

--- Class_Instance/Person.py
+++ Class_Instance/Person.py
@@ -24,12 +24,3 @@ class Person:
         age = age + 1
         return age
 
-t = int(input())
-for i in range(0, t):
-    age = int(input())
-    p = Person(age)
-    p.amIOld()
-    for j in range(0, 3):
-        p.yearPasses()
-    p.amIOld()
-    print("")
