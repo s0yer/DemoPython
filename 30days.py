b8a810422baf274d4599e70f3edd8c9445c61eea
jadsonmarliere <jds.mdo@gmail.com>
2019-11-05 12:11:12 -0300

    update
    List comprehensions

--- 30days.py
+++ 30days.py
@@ -76,16 +76,31 @@ if 1 <= n <= 1000:
 #----------------------------------------------------------------------
 
 # list comprehensions
+# basic sintax : [expr for item in lista if cond]
 
 x = 2
 y = 2
+z = 2
 n = 2
 
-print([i,j] for i in range(x+1) for j in range (y+1) if((i+j) != n))
+ar = [[i,j] for i in range(x+1) for j in range (y+1) if((i+j) != n)]
+print(ar)
+
+ar3 = [[i,j,k] for i in range(x+1) for j in range (y+1) for k in range(z+1) if((i+j+k) != n)]
+print(ar3)
 
 #----------------------------------------------------------------------
 
 # even and odd string print
+'''
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+
+'''
 
 t = int(input())
 even = []
