5b10cfc7d02b1af266a1fa13b4ec22b664da31f5
jadsonmarliere <jds.mdo@gmail.com>
2019-11-12 21:40:29 -0300

    tuple + hash

--- Funcoes.py
+++ Funcoes.py
@@ -446,3 +446,18 @@ def merge_the_tools(string, k):
 merge_the_tools(string, k)
 
 #------------------------------------------------------------------------------------
+
+# tuple + hash
+n = 2
+integer_list = [1 2]
+
+tupleN = tuple(integer_list)
+tupleN_hash = hash(tupleN)
+integer_list_hash = hash(integer_list)
+txt = str(tupleN_hash) + "=!" +  str(integer_list_hash)
+
+print(txt)
+
+
+#------------------------------------------------------------------------------------
+
