3547a413c05ae92f9201acec96187caa0cafefa1
jadsonmarliere <jds.mdo@gmail.com>
2019-11-25 22:34:45 -0300

    update

--- Funcoes.py
+++ Funcoes.py
@@ -496,24 +496,24 @@ list = []
 # number of operations
 n = 12
 i = 0
+elem = input()
 
 while i<n:
 
     if command == 'insert':
-
+        list.insert(elem)
     elif command == 'print':
         print(list)
-
     elif command == 'remove':
-
+        list.remove()
     elif command == 'append':
-
+        list.append(elem)
     elif command == 'sort':
-
+        list.sort()
     elif command == 'pop':
-
+        list.pop()
     elif command == 'reverse':
-
+        list.reverse()
     else:
         print('input a valid Command.')
 
