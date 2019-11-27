f5a7fdf8dd06b3df1caaf1022ec9c708d4ce8c5f
jadsonmarliere <jds.mdo@gmail.com>
2019-11-26 23:09:48 -0300

    update

--- Funcoes.py
+++ Funcoes.py
@@ -479,10 +479,12 @@ print(result)
 # Finding the percentage + dictionary
 
 n = 3
+
 student_marks = {}
 
 while i < n:
-
+    s = input()
+    split_and_join(s)
     i+=1
 
 average =  student_marks[name]/ n
