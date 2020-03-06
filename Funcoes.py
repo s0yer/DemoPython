3a3a23205dd99f49c3ee5981b6e4d75eb8abfb8d
jadsonmarliere <jds.mdo@gmail.com>
2020-03-06 00:05:01 -0500

    timeConversion

--- Funcoes.py
+++ Funcoes.py
@@ -149,20 +149,7 @@ def plusMinus(arr):
 
 
 #-------------------------------------------------------------------------------
-# Time conversion
-s = ['07:05:45PM']
-def timeConversion(s):
-    if s[-2:] == 'AM' and s[:2] == '12':
-        return '00' + s[2:-2]
 
-    elif s[-2:] == 'PM' and s[:2] == '12':
-        return s[:-2]
-
-    elif s[-2:] == 'AM':
-        return s[:-2]
-
-    else:
-        return s(int(s[:2])+12) + s[2:8]
 
 #-------------------------------------------------------------------------------
 # Birthday Cake Candles to blow out
