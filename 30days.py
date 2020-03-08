f4c8c918085ea2c50a4cbf59d12c0a3bef41a58e
jadsonmarliere <jds.mdo@gmail.com>
2020-03-07 23:44:36 -0500

    new package 30days
    2 functions

--- 30days.py
+++ 30days.py
@@ -1,35 +1,8 @@
-i = 4
-d = 4.0
-s = 'Canada is:  '
 
-
-ii = int(input())
-dd = float(input())
-ss = str(input())
-
-print(i+ii)
-print(d+dd)
-print(s+ss)
 
 #----------------------------------------------------------------------
 
-#odd and even
-N = 77
-def oddEven(N):
-    if 0 <= N <= 100:
-        if N % 2 == 0:
-            if 2 <= N <= 5:
-                print('Not Weird')
-            elif 6 <= N <= 20:
-                print('Weird')
-            else:
-                print('Not Weird')
-        else:
-            print('Weird')
-    else:
-        print('imput a valid number!')
 
-oddEven(N)
 
 #----------------------------------------------------------------------
 
---
+++ 30days/OddEven.py
@@ -0,0 +1,17 @@
+#odd and even
+N = 77
+def oddEven(N):
+    if 0 <= N <= 100:
+        if N % 2 == 0:
+            if 2 <= N <= 5:
+                print('Not Weird')
+            elif 6 <= N <= 20:
+                print('Weird')
+            else:
+                print('Not Weird')
+        else:
+            print('Weird')
+    else:
+        print('imput a valid number!')
+
+oddEven(N)
