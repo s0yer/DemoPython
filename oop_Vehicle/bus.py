10d61adf21d35e2f6f1f7a45d57620265cec75aa
jadsonmarliere <jds.mdo@gmail.com>
2020-05-13 06:32:42 -0300

    create vehicle.py
    classes
    Vehicle
    Bus
    Car

--- oop_Vehicle/bus.py
+++ oop_Vehicle/bus.py
@@ -1,9 +1,20 @@
-class Bus:
+from vehicle import Vehicle
 
-    def __init__(self):
-        self.passenger = []
+class Bus(Vehicle):
+
+    def __init__(self,start_speed=34):
+        # public atributes
+        self.top_speed_init = start_speed
+        self.observations = []
+        # private atribute
+        self.__secret_observations = []
+        self.trunk_size = 5
+        #atribute Bus
+        self.passengers = []
 
     def add_group(self, passengers):
         self.passengers.extend(passengers)
 
-Bus_candy = Bus()
+bus_candy = Bus(55)
+bus_candy.add_group([Jadson, Tania, Alberta, Helios])
+print(bus_candy.passengers)
--- oop_Vehicle/car.py
+++ oop_Vehicle/car.py
@@ -1,39 +1,9 @@
+from vehicle import Vehicle
+
 # blueprint class
 # encapsulation -> private and public
-class Car:
-    #class atribute (dont use atributes here, bad behavior)
-    top_speed = 100
-    warnings = []
+class Car(Vehicle):
 
-    #special metod, define atributes, converges to desired behavior
-    def __init__(self, start_speed = 55):
-        #public atributes
-        self.top_speed_init = start_speed
-        self.observations = []
-        #private atribute
-        self.__secret_observations = []
-        self.trunk_size = 5
-
-    # return de output / return -> need to be a string
-    def __repr__(self):
-        print('Special Methods!!!')
-        return 'Speed: {}, Observations:{}'.format(self.top_speed_init,len(self.observations))
-
-    # private method to add obs in the list -> __secret_observations
-    def add_secret_observation(self, observation_text):
-        if len(observation_text) > 0:
-            self.__secret_observations.append(observation_text)
-
-    # to access the value in secret_observations<private>
-    def get_observations(self):
-        return self.__secret_observations
-    #function access directly the class
-    def drive(self):
-        print('I am driving in the speed {}'.format(self.top_speed))
-
-    #function that change the atribute instantiated
-    def drive_init(self):
-        print('I am driving in the speed {}'.format(self.top_speed_init))
 
     def get_trunk_size(self):
         print('The size of the trunk of this vehicle is {}'.format(self.trunk_size))
