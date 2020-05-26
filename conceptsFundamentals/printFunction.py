fca169ea53adb53f85a5ed28a91cc2bfce2ef3ef
jadsonmarliere <jds.mdo@gmail.com>
2020-05-26 16:55:32 -0300

    Update printFunction.py

--- conceptsFundamentals/printFunction.py
+++ conceptsFundamentals/printFunction.py
@@ -2,9 +2,13 @@
 
 def printFunc():
 
-    n = int(input())
+    n = 8
+    print_list = ['printFunc()']
 
     i = 1
     while i <= n:
         print(i, end='')
+        print_list.append(i)
         i += 1
+
+    return print_list
