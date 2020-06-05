169e3f159661c3f13c9de3a7189169217354776a
jadsonmarliere <jds.mdo@gmail.com>
2020-06-05 00:35:12 -0300

    Update doorMat.py

--- JediDays/doorMat.py
+++ JediDays/doorMat.py
@@ -14,7 +14,12 @@ pattern:
 
 """
 def drawDoor():
+
+    list_log = ["drawDoor()"]
     n, m = map(int, input().split())
     pattern = [('.|.'*(2*i + 1)).center(m,'-') for i in range(n//2)]
     print('\n'.join(pattern + ['WELCOME'.center(m,'-')] + pattern [::-1]))
 
+    list_log.append([n,m,pattern])
+
+    return list_log
