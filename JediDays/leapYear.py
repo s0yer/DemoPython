3d0ec1e37f75cd2a0dbd8bce8ad71788e92b71eb
jadsonmarliere <jds.mdo@gmail.com>
2020-06-22 14:20:36 -0300

    update leapYear.py

--- JediDays/leapYear.py
+++ JediDays/leapYear.py
@@ -2,28 +2,29 @@
 
 def is_leap():
     list_log = ['is_leap()']
-    years_list = [1934,1955,1989,2001,2013,2021,2134]
-    year = 2000
-    list_log.append(year)
+    years_list = [1934,1955,1989,2000,2001,2013,2021,2134]
 
     leap = False
-    if 1900 <= year <= pow(10, 5):
-        if year % 4 == 0:
-            if year % 100 == 0:
-                if year % 400 == 0:
-                    leap = True
+    for year in years_list:
+        if 1900 <= year <= pow(10, 5):
+            if year % 4 == 0:
+                if year % 100 == 0:
+                    if year % 400 == 0:
+                        leap = True
+                    else:
+                        leap = False
                 else:
-                    leap = False
+                    leap = True
             else:
-                leap = True
+                leap = False
         else:
-            leap = False
-    else:
-        print('y out of range')
+            print('y out of range')
 
-    print(year)
-    print(leap)
-    list_log.append(leap)
+        print(year)
+        print(leap)
+        print('-------------------------------------------------')
+        ans = str(year) + ':' + str(leap)
+        list_log.append(ans)
 
     return list_log
 
