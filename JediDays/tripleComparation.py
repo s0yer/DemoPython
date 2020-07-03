5361613b9e21625211238011d47e19b00f4146ca
jadsonmarliere <jds.mdo@gmail.com>
2020-07-03 00:16:26 -0300

    Update tripleComparation.py

--- JediDays/tripleComparation.py
+++ JediDays/tripleComparation.py
@@ -4,21 +4,24 @@
 from random import randint
 
 def compareTriplets():
-    res = [0, 0]
-    a = [3,randint(1,8),2,4]
-    b = [randint(1,8),9,6,3]
+    a = [3,randint(1,10),5]
+    b = [randint(1,10),9,randint(1,10)]
 
+    a_point = 0
+    b_point = 0
     list_log = ['compareTriplets()']
     for i in range(3):
-        if a[i]>b[i]:
-            res[0] += 1
-        elif b[i]>a[i]:
-            res[1] += 1
+        if a[i] > b[i]:
+            a_point += 1
+        elif b[i] > a[i]:
+            b_point += 1
         else:
             print('Nobody earns a point!')
+
         list_log.append(a[i])
         list_log.append(b[i])
-        list_log.append(res)
+    res = [a_point, b_point]
+    list_log.append(res)
 
     return list_log
 
