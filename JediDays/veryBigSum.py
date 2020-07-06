82497677538989df219e0fea17b425f06a304723
jadsonmarliere <jds.mdo@gmail.com>
2020-07-06 07:52:51 -0300

    Update veryBigSum.py

--- JediDays/veryBigSum.py
+++ JediDays/veryBigSum.py
@@ -1,13 +1,28 @@
-# Very big sum
-# declaration of array
+# Python 3.7
+# 223
 
-#verificar entrada de dados, anteriormente setada em 10
+from random import randint
 
-ar = []
-# function with a for that walks in the array of size 'n'
-def aVeryBigSum(ar):
-    sum = 0
-    elem = 0
-    for elem in ar:
-        sum += elem
-    return sum
+# function with a for that walks in the array of size 'range(x,y)'
+def aVeryBigSum():
+
+    list_log = ['aVeryBigSum()']
+    arr = []
+    x = randint(1, 8)
+    y = randint(55, 89)
+    size_range = [x, y]
+    list_log.append(size_range)
+
+    for elem in range(x, y):
+        arr.append(randint(1, pow(2, 128)))
+
+    amount = 0
+    for elem in arr:
+        amount += elem
+    list_log.append(amount)
+    list_log.append(arr)
+
+    print('source list: ' + str(arr))
+    print('Sum of the list: ' + str(amount))
+
+    return list_log
