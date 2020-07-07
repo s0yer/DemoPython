16082da0f17c09b7ab7412ca9aa8f74f6e50e97d
jadsonmarliere <jds.mdo@gmail.com>
2020-07-07 10:56:33 -0300

    Create aidfunctions.py

---
+++ aidfunctions.py
@@ -0,0 +1,8 @@
+
+# input any quantity of variables and return a list of these elements
+def append_elements(*args):
+    data = []
+    for elem in args:
+        data.append(elem)
+
+    return data
