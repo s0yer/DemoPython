e11e4d7f272d043014767a16b61d5a5e5d0c250a
jadsonmarliere <jds.mdo@gmail.com>
2020-07-07 10:56:19 -0300

    Update tupleHash.py

--- JediDays/tupleHash.py
+++ JediDays/tupleHash.py
@@ -1,11 +1,26 @@
-# tuple + hash
+# Python 3.7
+# 224
 
-n = 2
-integer_list = [1 2]
+from random import randint
+import hashlib
+import aidfunctions
 
-tupleN = tuple(integer_list)
-tupleN_hash = hash(tupleN)
-integer_list_hash = hash(integer_list)
-txt = str(tupleN_hash) + "=!" +  str(integer_list_hash)
+def compare_tuple_list():
+    list_log = ['compare_tuple_list()']
+    # create a list of random integer numbers
+    random_list = []
+    for elem in range(1,10):
+        random_list.append(randint(1,55))
 
-print(txt)
+    # transform list -> tuple
+    tuple_random = tuple(random_list)
+
+    # take the hash
+    tuple_hash = hashlib.sha256(str(tuple_random).encode('utf-8')).hexdigest()
+    random_list_hash = hashlib.sha256(str(random_list).encode('utf-8')).hexdigest()
+
+    print('Hash of a tuple and list with the same elements: ')
+    print(str(tuple_random) + ' ' + tuple_hash)
+    print(str(random_list) + ' ' + random_list_hash)
+
+    return aidfunctions.append_elements('compare_tuple_list()', random_list, random_list_hash, tuple_random, tuple_hash)
