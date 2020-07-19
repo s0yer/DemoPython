7166318e73ece95243adefa7af1fe42aa7837ef3
jadsonmarliere <jds.mdo@gmail.com>
2020-07-19 10:57:20 -0300

    Update countValleys.py

--- JediDays/countValleys.py
+++ JediDays/countValleys.py
@@ -1,15 +1,26 @@
 
 def counting_valleys():
-    n = 10
-    s = 'UUDDUU'
+    n = 8
+    s = 'UUDDUDDD'
     ar = []
+    hike = 0
+    level_sea = 0
+    ar_hist = 0
+    valleys = 0
 
     if 2 <= n <= pow(10, 6):
         for elem in s:
             if elem == 'U' or elem == 'D':
+                if hike == 0:
+                    valleys = valleys + 1
+                else:
+                    ar_hist = ar_hist + 1
+
                 if elem == 'U':
+                    hike = hike + 1
                     ar.append("/")
                 else:
+                    hike = hike -1
                     ar.append("\\")
             else:
                 print("elem E {U,D} ")
@@ -19,5 +30,9 @@ def counting_valleys():
 
     else:
         print("input -> [2,10^6]")
+
+    print(hike)
+    print(valleys)
+    print(ar_hist)
 
 counting_valleys()
