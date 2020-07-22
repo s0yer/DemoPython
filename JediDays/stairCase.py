83d9fa5200ea4ff32aff675fdbe5cb67842512c6
jadsonmarliere <jds.mdo@gmail.com>
2020-07-22 20:04:26 -0300

    Update stairCase.py

--- JediDays/stairCase.py
+++ JediDays/stairCase.py
@@ -2,26 +2,27 @@
 # 211
 
 from random import randint
+import aidfunctions
 
 def staircase():
 
     n = randint(3,21)
-    list_log = ['staircase()', n]
+    size = n
     source = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '+']
     ch = source[randint(0,9)]
     model = ch
+    list_ans = []
 
     while n > 0:
         if n > 1:
             n -= 1
             print(' ' * (n - 1), ch)
             ch += model
-            list_log.append(ch)
-
+            list_ans.append(ch)
         else:
             print(ch)
             n -= 1
-            list_log.append(ch)
+            list_ans.append(ch)
 
-    return list_log
+    return aidfunctions.append_elements('staircase()', source, size, model, list_ans)
 
