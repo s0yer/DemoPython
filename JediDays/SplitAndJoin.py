9f53a27c9dde29c3b898cfc31696bad0d7b4f036
jadsonmarliere <jds.mdo@gmail.com>
2020-08-04 21:27:39 -0300

    Update SplitAndJoin.py

--- JediDays/SplitAndJoin.py
+++ JediDays/SplitAndJoin.py
@@ -1,23 +1,21 @@
 # Python 3.7
 # 218
-from random import  randint
+
+from random import randint
+import aidfunctions
 
 # change the spaces for especial characters
 def split_and_join():
-    list_log = ['split_and_join()']
 
     phrase = "To become the best you need to improve"
-    list_log.append(phrase)
 
     # change space -> -
     s_split = phrase.split(" ")
     v_trace = "-".join(s_split)
-    list_log.append(v_trace)
 
     # change - -> +
     t_split = v_trace.split("-")
     k_plus = "+".join(t_split)
-    list_log.append(k_plus)
 
     # shuffle the source phrase - on going
     source_split = phrase.split(" ")
@@ -26,7 +24,7 @@ def split_and_join():
         elem = source_split.pop(randint(0,len(source_split)-1))
         shuffle_list.append(elem)
     new_shuffle_phrase = ' '.join(shuffle_list)
-    list_log.append(new_shuffle_phrase)
+
     # test if the loop is working and if the list is empty
     if len(new_shuffle_phrase) == 0:
         new_shuffle_phrase = 'on going'
@@ -42,7 +40,6 @@ def split_and_join():
         bonus_list.append(s_split[i])
         size_new_phrase = len(bonus_list)
     new_phrase = ' '.join(bonus_list)
-    list_log.append(new_phrase)
 
     print('source: ' + phrase)
     print('swap -: ' + str(v_trace))
@@ -50,6 +47,6 @@ def split_and_join():
     print('bonus phrase(shuffle): ' + new_shuffle_phrase)
     print('bonus phrase(disorder): ' + new_phrase)
 
+    return aidfunctions.append_elements('split_and_join()', phrase, v_trace, k_plus, new_shuffle_phrase, new_phrase)
 
-    return list_log
 
