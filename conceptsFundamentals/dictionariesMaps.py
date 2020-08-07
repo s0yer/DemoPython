29c6a510c05448b0e20d5daa51b6103e7d87f505
jadsonmarliere <jds.mdo@gmail.com>
2020-08-07 07:41:43 -0300

    Update dictionariesMaps.py

--- conceptsFundamentals/dictionariesMaps.py
+++ conceptsFundamentals/dictionariesMaps.py
@@ -1,25 +1,41 @@
-# on going
+# Python 3.7
+# 003
 
-def dictMaps():
+import aidfunctions
+from random import randint
 
-    consult = "Kira"
+def create
+
+def dict_maps():
+    names = ['avocado', 'watermelon', 'ginger', 'pepper', 'cinnamon']
+    list_numbers = []
+    size = len(names)
+    for i in range(0, size):
+        list_numbers.append(randint(0, 1000))
+
+    source_list = []
+    for i in range(0, size):
+        elem = str(names[i]) + ' ' + str(list_numbers[i])
+        source_list.append(elem)
+
+    consult = names[randint(0, len(names)-1)]
     s = "Kira 551254358"
     # n = int(input().strip())
     n = 4
+    print(list_numbers, source_list, consult)
+
     dictPhones = {}
-
-
     # populate the dictionary
-    for el in range(n):
+    for el in range(size):
         # name, num = input().strip().split(' ')
-        name, num = s.strip().split(' ')
+        name, num = source_list[el].strip().split(' ')
         dictPhones[name] = num
 
     # query dictPhones while there is input, otherwise exit when EOF (except)
     # while (True):
-    i=0
 
-    while (i < 5):
+    i = 0
+    while (i < size):
         try:
             # queryName = input().strip()
             queryName = consult.strip()
@@ -34,7 +50,9 @@ def dictMaps():
 
         i+=1
     else:
-        print("consulted 5 times, end...")
+        print("consulted {} times, end...".format(size))
 
-    list_answer = ['dictMaps()', x]
-    return list_answer
+    print(names, )
+    return aidfunctions.append_elements('dict_maps()', x)
+
+dict_maps()
