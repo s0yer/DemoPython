c27a834d6ddf384742328d813a9464f9d9abeb76
jadsonmarliere <jds.mdo@gmail.com>
2020-08-09 10:30:39 -0300

    Update dynamicDescompression.py

--- conceptsFundamentals/dynamicDescompression.py
+++ conceptsFundamentals/dynamicDescompression.py
@@ -1,10 +1,25 @@
+# Python 3.7
+# 004
 
-def dynamicDesc():
+from random import randint
+from aidfunctions import append_elements
 
-    a = [3,6,9]
 
-    #descompactação dinâmica / dynamic decompression
+def dynamic_descompression():
+
     # dynamic decompression
-    b = ['dynamicDesc() ', 'elementos : {} {} {}'.format(*a)]
-    print(b)
-    return b
+    a = [5, 6, 9]
+    ans = 'Elements example 1 :' + ' {} {} {}'
+    print(ans.format(*a))
+
+    # example 2
+    size = randint(5, 21)
+    source_list = []
+    # create a random list
+    for i in range(0, size):
+        source_list.append(randint(55, 89))
+
+    ans_2 = 'Elements example 2: ' + size * ' {} '
+    print(ans_2.format(*source_list))
+
+    return append_elements('dynamic_descompression()', ans, ans_2)
