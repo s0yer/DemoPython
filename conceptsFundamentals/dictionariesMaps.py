01cdb68b8c0a7f352001588a814a327844a43fd8
jadsonmarliere <jds.mdo@gmail.com>
2020-08-08 23:17:52 -0300

    Update dictionariesMaps.py

--- conceptsFundamentals/dictionariesMaps.py
+++ conceptsFundamentals/dictionariesMaps.py
@@ -4,7 +4,9 @@
 import aidfunctions
 from random import randint
 
-def create
+# on going
+def create_list():
+    return 0
 
 def dict_maps():
     names = ['avocado', 'watermelon', 'ginger', 'pepper', 'cinnamon']
@@ -18,41 +20,37 @@ def dict_maps():
         elem = str(names[i]) + ' ' + str(list_numbers[i])
         source_list.append(elem)
 
-    consult = names[randint(0, len(names)-1)]
-    s = "Kira 551254358"
-    # n = int(input().strip())
-    n = 4
-    print(list_numbers, source_list, consult)
 
-    dictPhones = {}
+    print('Source: {} '.format(source_list))
+
+    dict_phones = {}
     # populate the dictionary
     for el in range(size):
-        # name, num = input().strip().split(' ')
+        # detach the elements of source_list
         name, num = source_list[el].strip().split(' ')
-        dictPhones[name] = num
+        dict_phones[name] = num
 
-    # query dictPhones while there is input, otherwise exit when EOF (except)
+    # query dict_phones while there is input, otherwise exit when EOF (except)
     # while (True):
-
+    times_consult = randint(34, 55)
     i = 0
-    while (i < size):
+    while i < times_consult:
         try:
-            # queryName = input().strip()
-            queryName = consult.strip()
-            if queryName in dictPhones:
-                print('{}={}'.format(queryName, dictPhones[queryName]))
-                x = '{}={}'.format(queryName, dictPhones[queryName])
+            consult = names[randint(0, len(names) - 1)]
+            query_name = consult.strip()
+            if query_name in dict_phones:
+                print('{} = {}'.format(query_name, dict_phones[query_name]))
+                x = '{} = {}'.format(query_name, dict_phones[query_name])
             else:
                 print('Not found')
 
         except EOFError:
             print("error...")
 
-        i+=1
+        i += 1
     else:
         print("consulted {} times, end...".format(size))
 
     print(names, )
-    return aidfunctions.append_elements('dict_maps()', x)
+    return aidfunctions.append_elements('dict_maps()', source_list, x)
 
-dict_maps()
