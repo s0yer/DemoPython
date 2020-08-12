4721327e7afac71494294b84f59f457ed5fa5407
jadsonmarliere <jds.mdo@gmail.com>
2020-08-11 21:27:16 -0300

    Update SimpleArraySum.py

--- JediDays/SimpleArraySum.py
+++ JediDays/SimpleArraySum.py
@@ -1,22 +1,21 @@
 # Simple array sum
+#
 from random import randint
-
+import aidfunctions
 
 def simpleArraySum():
-    list_log = ['simpleArraySum()']
-    size_list = randint(1,2000)
+
+    size_list = randint(1, 2000)
     r_list = []
     i = 0
     while i < size_list:
-        r_list.append(randint(1,500))
+        r_list.append(randint(1, 500))
         i += 1
 
     sum = 0
     for elem in r_list:
         sum += elem
 
-    list_log.append(size_list)
-    list_log.append(sum)
+    return aidfunctions.append_elements('simpleArraySum()', size_list, sum)
 
-    return list_log
 
