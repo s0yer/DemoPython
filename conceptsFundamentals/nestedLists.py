f5ac8539c5d017282126cc971a0a9bf647ac98e2
jadsonmarliere <jds.mdo@gmail.com>
2020-08-12 04:15:48 -0300

    Update nestedLists.py

--- conceptsFundamentals/nestedLists.py
+++ conceptsFundamentals/nestedLists.py
@@ -1,22 +1,21 @@
-"""
-It is also possible to use the set() constructor to make a set.
+# Python 3.7
+# 006
+# It is also possible to use the set() constructor to make a set.
 
-"""
 import random
+from aidfunctions import append_elements
 
-def nestedL():
 
-    listSheet = []
+def nested_list():
+    list_sheet = []
     i = 0
-    name_list = ['Keira', 'nissa', 'triss', 'lilian']
-    for el in range(0,4):
-        listSheet.append([name_list[i],float(random.random()*21)])
-        i+=1
+    name_list = ['kira', 'nissa', 'triss', 'lilian']
+    for el in range(0, 4):
+        list_sheet.append([name_list[i], float(random.random()*21)])
+        i += 1
 
-    secondHighest = sorted(list(set([elem for name, elem in listSheet])))[1]
-    print('\n'.join([a for a,b in sorted(listSheet) if b == secondHighest]))
+    second_highest = sorted(list(set([elem for name, elem in list_sheet])))[1]
+    print('Source List: ' + str(list_sheet))
+    print('\n'.join([a for a, b in sorted(list_sheet) if b == second_highest]))
 
-    list_answer = ['nestedL()']
-    list_answer.append(listSheet)
-
-    return list_answer
+    return append_elements('nested_list()', list_sheet)
