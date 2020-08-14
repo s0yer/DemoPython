240c02ab3b0a9e723118afe8934c5241b05fdc72
jadsonmarliere <jds.mdo@gmail.com>
2020-08-13 22:12:08 -0300

    Update tupleDictionary.py

--- conceptsFundamentals/tupleDictionary.py
+++ conceptsFundamentals/tupleDictionary.py
@@ -1,21 +1,23 @@
+# Python 3.7
+# 010
 # tuple/dictionary
 
-def ArgumentosDicionario(*arg, **keywordArgs):
-    list_args = ['ArgumentosDicionario()']
+
+def args_dictionary(*arg, **keywordArgs):
+    list_args = ['args_dictionary()']
     print(keywordArgs)
+
     for k, argument in keywordArgs.items():
         print(k, argument)
 
     for elem in arg:
         print(elem)
 
-
         list_args.append([elem])
 
     return list_args
 
-#dicionarios / Dictionaries
 
-def callArgDic():
-
-    ArgumentosDicionario(1, 1, 2, 3, 5, nome='Jadson', idade='27')
+# Dictionary
+def call_args_dict():
+    args_dictionary(1, 1, 2, 3, 5, nome='Jadson', idade='27')
