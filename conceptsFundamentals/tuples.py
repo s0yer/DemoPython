cac07788932c4b7254301bedca5001ab840e8910
jadsonmarliere <jds.mdo@gmail.com>
2020-08-13 22:11:50 -0300

    Update tuples.py

--- conceptsFundamentals/tuples.py
+++ conceptsFundamentals/tuples.py
@@ -1,15 +1,28 @@
-#tuple
-def ArgumentosInfinitos(*args):
-    args_list = ['ArgumentosInfinitos']
+# Python 3.7
+# 011
+# Tuples
+
+from aidfunctions import append_elements
+from random import randint
+
+
+def infinite_arguments(*args):
+
     print(args)
-    for elemento in args:
-        print(elemento)
+    for elem in args:
+        print(elem)
 
     x = list(args)
-    args_list.append(x)
-    return args_list
 
-#tuple
-def callArgInf():
-    ArgumentosInfinitos(1,2,3,4)
-    ArgumentosInfinitos(*[3,3,3,3])
+    return append_elements('infinite_arguments', args, x)
+
+
+def call_infinite_args():
+    infinite_arguments(1, 2, 3, 4)
+    infinite_arguments(*[3, 3, 3, 3])
+
+    random_list = []
+    for i in range(0, randint(21, 55)):
+        random_list.append(randint(55, 89))
+
+    infinite_arguments(*random_list)
