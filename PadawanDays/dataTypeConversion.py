031b96961bd1841cf6b3a301cc47ad5eb3f72764
jadsonmarliere <jds.mdo@gmail.com>
2020-08-15 10:12:44 -0300

    Update dataTypeConversion.py

--- PadawanDays/dataTypeConversion.py
+++ PadawanDays/dataTypeConversion.py
@@ -1,22 +1,27 @@
 # python 3.7
+# 101
 
-def dtConversion():
-
-    dtConv_list = ['dtConversion()']
-    i = 21.13853211
-
-    inteiro = int(i)
-    flutuante = float(i)
-    corda = str(i)
-    dtConv_list.append([[inteiro,flutuante,corda],[type(inteiro), type(flutuante), type(corda)],[pow(i, 2)]])
-
-    print(inteiro)
-    print(type(inteiro))
-    print(flutuante)
-    print(type(flutuante))
-    print(corda)
-    print(type(corda))
+from random import randint
+from aidfunctions import append_elements
 
 
+def datatype_conversion():
+    dt_con_list = []
+    source_list = []
+    for _ in range(0, 3):
+        source_list.append(randint(55, 89))
 
-    return dtConv_list
+    integer_number = int(source_list[0])
+    float_number = float(source_list[1])
+    string_number = str(source_list[2])
+    dt_con_list.append([[integer_number, float_number, string_number], [type(integer_number), type(float_number),
+                                                                        type(string_number)]])
+
+    print(integer_number)
+    print(type(integer_number))
+    print(float_number)
+    print(type(float_number))
+    print(string_number)
+    print(type(string_number))
+
+    return append_elements('datatype_conversion()', dt_con_list)
