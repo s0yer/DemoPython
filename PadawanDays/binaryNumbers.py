303570570ad48dfd2d0e285ea322fc5639a47746
jadsonmarliere <jds.mdo@gmail.com>
2020-08-17 10:10:12 -0300

    Update binaryNumbers.py

--- PadawanDays/binaryNumbers.py
+++ PadawanDays/binaryNumbers.py
@@ -1,26 +1,31 @@
 # python 3.7
+# 102
 
 import os
 import random
 import re
 import sys
 
+from random import randint
+from aidfunctions import append_elements
 
-def binaryConversion():
+def binary_conversion():
 
-    binary_list = ['binaryConversion()']
     print("input: n -> [1,1000000] ")
-    # n = int(input().strip())
-    n = 252121
+    n = randint(500000, 1500000)
+    print('Source number: {}'.format(n))
 
     if 1 <= n <= 1000000:
         numbers = str(bin(n)[2:]).split('0')
-        lenghts = [len(num) for num in numbers]
-        print(max(lenghts))
-        binary_list.append([numbers,lenghts])
-        return binary_list
+        lengths = [len(num) for num in numbers]
+        ans = [numbers, lengths]
+
+        print('Length max: ' + str(max(lengths)))
+        print('Numbers: ' + str(ans[0]))
+        print('Lengths: ' + str(ans[1]))
 
     else:
-        print("n -> [1,1000000]")
-        binary_list.append('Error : n -> [1,1000000]')
-        return binary_list
+        ans = 'Error : n -> [1,1000000]'
+        print(ans)
+
+    return append_elements('binary_conversion()', n, ans)
