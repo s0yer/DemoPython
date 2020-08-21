29cb100b792578da41bebd4b6e132ec86dda8521
jadsonmarliere <jds.mdo@gmail.com>
2020-08-20 22:41:19 -0300

    Update listComprehensions.py

--- PadawanDays/listComprehensions.py
+++ PadawanDays/listComprehensions.py
@@ -1,25 +1,29 @@
+# Python 3.7
+# 105
 # list comprehensions
-# basic sintax : [expr for item in lista if cond]
+# basic sintax : [expr for item in list if cond]
 
 
+from aidfunctions import append_elements
+from random import randint
 
-def listComp():
+def list_comp():
 
-    list_log = ['listComp()']
-    x = 2
-    y = 2
-    z = 2
-    n = 2
+    w = 3
+    x = randint(1, w)
+    y = randint(1, w)
+    z = randint(1, w)
+    n = randint(1, w)
+    print(x)
+    print(y)
+    print(z)
+    print(n)
 
-    ar = [[i,j] for i in range(x+1) for j in range (y+1) if((i+j) != n)]
+    ar = [[i, j] for i in range(x + 1) for j in range(y + 1) if ((i + j) != n)]
     print(ar)
 
-    ar3 = [[i,j,k] for i in range(x+1) for j in range (y+1) for k in range(z+1) if((i+j+k) != n)]
+    ar3 = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if ((i + j + k) != n)]
     print(ar3)
 
-    list_log.append('ar')
-    list_log.append(ar)
-    list_log.append('ar3')
-    list_log.append(ar3)
+    return append_elements('list_comp()', x, y, z, n, 'ar: ' + str(ar), 'ar3: ' + str(ar3))
 
-    return list_log
