119278249e88c05f03f7fae666d14b5868b9a456
jadsonmarliere <jds.mdo@gmail.com>
2020-08-21 20:36:38 -0300

    Update oddEven.py

--- PadawanDays/oddEven.py
+++ PadawanDays/oddEven.py
@@ -1,32 +1,39 @@
-#odd and even
+# Python 3.7
+# 107
+# odd and even
 
-def oddEven():
-    list_numbers = [-5,0,100,5,21,55,89,2,1,4,7,14,64,92,47,200,400]
-    list_log = ['oddEven()']
-    list_log.append(list_numbers)
+from aidfunctions import append_elements
+from random import randint
+
+def odd_even():
+
+    list_numbers = []
+    for elem in range(21, randint(34, 55)):
+        list_numbers.append(randint(-89, 89))
+
+    answer_list = []
     print(list_numbers)
 
-    for el in list_numbers:
-        if 0 <= el <= 100:
-            if el % 2 == 0:
-                if 2 <= el <= 5:
+    for elem in list_numbers:
+
+        if 0 <= elem <= 100:
+            if elem % 2 == 0:
+
+                if 2 <= elem <= 5:
                     txt_ans = 'Weird (even)'
-                    print(str(el) +' '+ txt_ans)
-                elif 6 <= el <= 20:
+                elif 6 <= elem <= 20:
                     txt_ans = 'Not Bad (even)'
-                    print(str(el) +' '+ txt_ans)
                 else:
                     txt_ans = 'ok (even)'
-                    print(str(el) +' '+ txt_ans)
 
             else:
                 txt_ans = '(odd)'
-                print(str(el) +' '+txt_ans)
         else:
-            txt_ans = 'imput a valid number! [0,100]'
-            print(str(el) +' '+txt_ans)
+            txt_ans = 'input a valid number! [0,100]'
 
-        list_log.append(el)
-        list_log.append(txt_ans)
+        ans = str(elem) + ': ' + txt_ans
+        print(ans)
+        answer_list.append(ans)
 
-    return list_log
+    return append_elements('odd_even()', list_numbers, answer_list)
+
