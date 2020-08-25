a8e7fda59fa47236016cadad5a1ebfab6a66f4e5
jadsonmarliere <jds.mdo@gmail.com>
2020-08-25 10:38:38 -0300

    Update filterlist.py

--- PadawanDays/filterlist.py
+++ PadawanDays/filterlist.py
@@ -2,5 +2,16 @@
 # 115
 
 # input a list and filter neg and positive numbers
+from random import randint
 
 
+def create_source_list():
+
+    source_list = []
+    size = randint(21, 34)
+    for elem in range(size):
+        source_list.append(randint(-144, 144))
+
+    return source_list
+
+print(create_source_list())
