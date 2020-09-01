989a096b14b1ac8817fa31231adb041357d05acd
jadsonmarliere <jds.mdo@gmail.com>
2020-09-01 05:15:17 -0300

    Update ListDictionary.py

--- conceptsFundamentals/ListDictionary.py
+++ conceptsFundamentals/ListDictionary.py
@@ -1,30 +1,51 @@
+# Python 3.7
+# 005
 
-def listDictionary():
+from aidfunctions import append_elements
 
-    lista = [1,2,3,4,5]
+def list_dictionary_tuple():
+
+    # LIST ----------------------------
+    source_list = [1,2,3,4,5]
+    lista = source_list.copy()
     lista.extend([6,7,8,9])
     del(lista[0])
+    print('---------------------------------')
+    print('SOURCE LIST : {}'.format(str(source_list)))
     print(lista)
 
+    # DICTIONARY --------------------------------
+    # consult item
     d = {'nome': 'Jadson'}
     print(d.items())
+    print('---------------------------------')
+    print('SOURCE DICTIONARY : {}'.format(str(d)))
 
+    # printing item in a list
     for k, v in d.items():
         print(k, v)
 
+    # delete element in a dictionary
     del(d['nome'])
     print(d)
 
+    # TUPLE ------------------------------
     t = (1,1,2,3,5,8,13,21)
+    print('---------------------------------')
+    print('SOURCE TUPLE : {}'.format(str(t)))
     print(t.index(5))
     print(t[4])
     print(t[t.index(5)])
 
-    def veficicaIndice():
-        if t[4] == t[t.index(5)]:
-            return True
-        else:
-            return False
+    return append_elements('list_dictionary_tuple()', lista)
+
+def veficicaIndice():
+
+    if t[4] == t[t.index(5)]:
+        return True
+    else:
+        return False
+
     print(veficicaIndice())
 
     print(t)
@@ -32,9 +53,9 @@ def listDictionary():
     s ={'Jadson', 'Yenn', 'Triss'}
     print(s)
     print('-------------------------------------------------------------------------------')
-    list_answer = ['listDictionary()',lista, d, t.index(3), veficicaIndice(), s]
+    list_answer = ['list_dictionary_tuple()',lista, d, t.index(3), veficicaIndice(), s]
     print(list_answer)
     print('-------------------------------------------------------------------------------')
 
 
-    return list_answer
+list_dictionary_tuple()
