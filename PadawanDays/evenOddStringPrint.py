4498b7cba72f113a25818ff796ae75a2173018cc
jadsonmarliere <jds.mdo@gmail.com>
2020-08-31 21:24:11 -0300

    Update evenOddStringPrint.py

--- PadawanDays/evenOddStringPrint.py
+++ PadawanDays/evenOddStringPrint.py
@@ -1,5 +1,5 @@
 # Python 3.7
-# even and odd string print
+# even and odd source_text print
 # 104
 
 from aidfunctions import append_elements
@@ -7,33 +7,27 @@ from aidfunctions import append_elements
 def even_odd_string():
 
     t = 1
-    even = []
-    odd = []
+    even_list = []
+    odd_list = []
     source_word = "wisdomisclearandprecise"
 
     if 1 <= t <= 10:
         while t > 0:
 
             i = 0
-            # size_s = len(source_word)
-            # while i < size_s:
-            #     if i % 2 == 0:
-            #         even.append(source_word[i])
-            #     else:
-            #         odd.append(source_word[i])
-            #     i += 1
+
             for el in source_word:
                 if (i % 2) == 0:
-                     even.append(el)
+                     even_list.append(el)
                 else:
-                     odd.append(el)
+                     odd_list.append(el)
                 i += 1
             t -= 1
 
-        for el in even:
+        for el in even_list:
             print(el, end='\n')
         print('-----------------------------')
-        for el in odd:
+        for el in odd_list:
             print(el, end='\n')
         print('-----------------------------')
 
@@ -42,4 +36,4 @@ def even_odd_string():
         ans = 't needs to be [1,10]'
         print(ans)
 
-    return append_elements('even_odd_string()', source_word, even, odd, ans)
+    return append_elements('even_odd_string()', source_word, even_list, odd_list, ans)
