3f4421e64c1aafeb96a55a4429d59361d9760ab6
jadsonmarliere <jds.mdo@gmail.com>
2020-09-04 05:22:44 -0300

    Update ListDictionary.py

--- conceptsFundamentals/ListDictionary.py
+++ conceptsFundamentals/ListDictionary.py
@@ -2,14 +2,27 @@
 # 005
 
 from aidfunctions import append_elements
+from random import randint
+
+
+def check_index(arr, id):
+    if arr[id] > 0:
+        print('Element is bigger than 0'.format(arr[id]))
+        return True
+    elif arr[id] < 0:
+        print('Element is smaller than 0')
+        return True
+    else:
+        print('The element is 0')
+        return False
+
 
 def list_dictionary_tuple():
-
     # LIST ----------------------------
-    source_list = [1,2,3,4,5]
+    source_list = [1, 2, 3, 4, 5]
     lista = source_list.copy()
-    lista.extend([6,7,8,9])
-    del(lista[0])
+    lista.extend([6, 7, 8, 9])
+    del (lista[0])
     print('---------------------------------')
     print('SOURCE LIST : {}'.format(str(source_list)))
     print(lista)
@@ -21,41 +34,35 @@ def list_dictionary_tuple():
     print('---------------------------------')
     print('SOURCE DICTIONARY : {}'.format(str(d)))
 
-    # printing item in a list
+    # printing item in a arr
     for k, v in d.items():
         print(k, v)
 
     # delete element in a dictionary
-    del(d['nome'])
+    del (d['nome'])
     print(d)
 
     # TUPLE ------------------------------
-    t = (1,1,2,3,5,8,13,21)
+    t = (1, 1, 2, 3, 5, 8, 13, 21)
     print('---------------------------------')
     print('SOURCE TUPLE : {}'.format(str(t)))
     print(t.index(5))
     print(t[4])
     print(t[t.index(5)])
 
-    return append_elements('list_dictionary_tuple()', lista)
+    # Random list and check the element
+    print('------------------------------')
+    print('CHECK ELEMENT OF LIST: ')
+    random_list = []
+    for el in range(randint(21, 34)):
+        random_list.append(randint(-144, 144))
+    index_to_check = randint(0, (len(random_list) - 1))
 
-def veficicaIndice():
+    print('Source list: ' + str(random_list))
+    print('Index to check: ' + str(index_to_check))
+    ans_check = check_index(random_list, index_to_check)
 
-    if t[4] == t[t.index(5)]:
-        return True
-    else:
-        return False
-
-    print(veficicaIndice())
-
-    print(t)
-
-    s ={'Jadson', 'Yenn', 'Triss'}
-    print(s)
-    print('-------------------------------------------------------------------------------')
-    list_answer = ['list_dictionary_tuple()',lista, d, t.index(3), veficicaIndice(), s]
-    print(list_answer)
-    print('-------------------------------------------------------------------------------')
+    return append_elements('list_dictionary_tuple()', lista, d, t, index_to_check, ans_check)
 
 
 list_dictionary_tuple()
