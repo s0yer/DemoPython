607a145155df624d1893b4d43860925376133003
jadsonmarliere <jds.mdo@gmail.com>
2020-09-11 11:29:36 -0300

    Create breakingrecords.py

---
+++ ongoing/breakingrecords.py
@@ -0,0 +1,14 @@
+# Python
+
+from random import randint
+
+def breaking_records():
+    size = 7
+
+    if 1 <= size <= pow(10, 3):
+
+        print('ok')
+    else:
+        print('size -> [1, 10e3]')
+
+breaking_records()
