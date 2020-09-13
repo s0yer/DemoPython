523f7b4177e2683f218d61f148df483a0cd841b9
jadsonmarliere <jds.mdo@gmail.com>
2020-09-13 19:06:35 -0300

    Update breakingrecords.py

--- ongoing/breakingrecords.py
+++ ongoing/breakingrecords.py
@@ -14,6 +14,11 @@ def breaking_records(n):
     # test quantity of records
     if 1 <= size <= pow(10, 3):
         print('size ok')
+        if size < 2:
+            print('Only one element in the records, max and min = {}'.format(source_list[0]))
+            return True
+        else:
+            print('Continue process...')
     else:
         print('size -> [1, 10e3]')
         return False
@@ -25,6 +30,30 @@ def breaking_records(n):
         print('size -> [1, 10e8]')
         return False
 
+    # if the constrains is ok, declaration of new variables
+    max_score = source_list[0]
+    min_score = source_list[0]
+    times_beat_max = 0
+    times_beat_min = 0
+
+    # brain
+    for elem in source_list:
+        if max_score < elem:
+            max_score = elem
+            times_beat_max += 1
+        elif min_score > elem:
+            min_score = elem
+            times_beat_min += 1
+        else:
+            print('Everthing stay the same, score: {}'.format(elem))
+
+    print('------------------------')
+    print('Score max : {}'.format(max_score))
+    print('Max score beated {} times!'.format(times_beat_max))
+    print('------------------------')
+    print('Score min: : {}'.format(min_score))
+    print('Min score beated {} times!'.format(times_beat_min))
+    print('------------------------')
     print('end... ok process!')
 
 
