4f03fb4127c9d09816cd974ae735fb4c0cca88ce
jadsonmarliere <jds.mdo@gmail.com>
2020-09-14 08:16:21 -0300

    Update divisiblesumpairs.py

--- ongoing/divisiblesumpairs.py
+++ ongoing/divisiblesumpairs.py
@@ -3,9 +3,14 @@
 def div_sum_pairs():
     # size array [2, 100]
     size_array = 5
-    
+    if 2 <= size_array <= 100:
+        print('size array ok.')
+    else:
+        return False
+
     # element key divisible [1, 100]
-    div_key = 3
-
-    # a[i] -> [1, 100]
-
+    key_divider = 3
+    if 1 <= key_divider <= 100:
+        print('key divider ok.')
+    else:
+        return False
