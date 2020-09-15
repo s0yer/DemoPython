a7e8f1c4d728e1da66488a1a0f3f21c520d411f2
jadsonmarliere <jds.mdo@gmail.com>
2020-09-15 07:15:24 -0300

    Update between2sets.py

--- ongoing/between2sets.py
+++ ongoing/between2sets.py
@@ -9,11 +9,28 @@ def source_data():
 
 def brain_between2sets():
 
-    arr_a, arr_b = source_data()
+    list_a, list_b = source_data()
 
-    if 1 <= len(arr_a) and len(arr_b) <= 10:
+    # Constraints test
+    if 1 <= len(list_a) and len(list_b) <= 10:
         print(True)
+        for elem in list_a:
+            if elem < 1 or elem > 100:
+                return False
+            else:
+                print('list A ok.')
+
+        for elem in list_b:
+            if elem < 1 or elem > 100:
+                return False
+            else:
+                print('list B ok.')
+        print('Pass for the next process...')
     else:
         return False
-
+    count_div = 0
+    for elem_a in list_a:
+        for elem_b in list_b:
+            if elem_b % elem_a = 0:
+                count_div += 1
 brain_between2sets()
