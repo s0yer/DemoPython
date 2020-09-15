7ffa6adf6c1667498638c254529d20b36e7b253d
jadsonmarliere <jds.mdo@gmail.com>
2020-09-15 07:20:51 -0300

    Create jumpingclouds.py

---
+++ ongoing/jumpingclouds.py
@@ -0,0 +1,11 @@
+# Python 3.7
+
+def jump_cloud():
+
+    # number of clouds -> integer [2, 100]
+    n_clouds = [5]
+
+    # elements of sky_list -> integer [0,1]
+    sky_list = []
+
+    # the navigator need to jump the ones[1] cloud
