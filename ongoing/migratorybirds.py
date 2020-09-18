9777ebf9b89d5dcba3c1ec6f166e1cd75c139051
jadsonmarliere <jds.mdo@gmail.com>
2020-09-18 12:29:46 -0300

    Update migratorybirds.py

--- ongoing/migratorybirds.py
+++ ongoing/migratorybirds.py
@@ -1,5 +1,24 @@
 # Python 3.7
 
+from random import randint
 
-def most_commum_bird():
+def most_commum_bird(max_size):
     birds_types_list = []
+
+    for elem in range(1000, randint(10000, max_size)):
+        birds_types_list.append(randint(1, 34))
+    print('bird list {} created '.format(len(birds_types_list)))
+
+    if 5 <= len(birds_types_list) <= 2 * pow(10, 5):
+        print('Birds list ok.')
+    else:
+        print('Birds list NOT OK.')
+        print()
+        return False
+
+
+for i in range(30):
+    print('processing... {}'.format(i))
+    max_size = pow(10, randint(4, 8))
+    most_commum_bird(max_size)
+    print('End processing {}'.format(i))
