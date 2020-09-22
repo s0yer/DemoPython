6c39552d1c1a51ab24b7833472b318de89e0898e
jadsonmarliere <jds.mdo@gmail.com>
2020-09-22 19:41:06 -0300

    Update jumpingclouds.py

--- ongoing/jumpingclouds.py
+++ ongoing/jumpingclouds.py
@@ -1,19 +1,51 @@
 # Python 3.7
 
-def jump_cloud():
+from random import randint
 
+
+def jump_cloud():
+    # create a souce list of clouds
+    clouds = [0]
+    for elem in range(randint(55, 89)):
+        clouds.append(randint(0, 1))
+
+    # cloud origin need  to be  0
+    if clouds[0] == 0:
+        print('First cloud ok.')
+    else:
+        print('First element not ok.')
+
+    size_sky = len(clouds)
     # number of clouds -> integer [2, 100]
-    n_clouds = [5]
-    if 2 <= n_clouds <= 100:
+    if 2 <= size_sky <= 100:
         print('Numbers of clouds ok.')
     else:
         return False
 
     # elements of sky_list -> integer [0,1]
-    sky_list = []
-    if 0 <= sky_list <= 1:
-        print('clouds ok.')
-    else:
-        return False
+    for elem in clouds:
+        if 0 <= elem <= 1:
+            print('cloud {} ok.'.format(elem))
+        else:
+            return False
 
     # the navigator need to jump the ones[1] cloud
+    jumps = 0
+    i = 1
+    while i < size_sky:
+        if clouds[i] == 0:
+            print('jump: cloud {} -> cloud {}'.format(jumps, i))
+            jumps = i
+        else:
+            print('Not jump to {}'.format(i))
+
+        i += 1
+
+    print('-------------------------------')
+    print('Quantity of clouds {}'.format(size_sky))
+    print('Quantity of jumps {}'.format(jumps))
+
+    return True
+
+
+jump_cloud()
