1dd98d1924f789b257e9ed04fe785746f5d9205b
jadsonmarliere <jds.mdo@gmail.com>
2020-09-24 06:05:57 -0300

    Update turnpages.py

--- ongoing/turnpages.py
+++ ongoing/turnpages.py
@@ -5,6 +5,8 @@ from random import randint
 def seek_page():
     size_book = randint(-9, 90)
     wanted_page = randint(-5, size_book)
+    print('size book: {}'.format(size_book))
+    print('wanted page: {}'.format(wanted_page))
 
     if 1 <= size_book <= pow(10, 5):
         print('Size book ok.')
@@ -20,7 +22,31 @@ def seek_page():
         print('-------------')
         return False
 
+    source_book = []
+    i = 1
+    for _ in range(size_book):
+        source_book.append(i)
+        i += 1
+    print(source_book)
+    print('------------')
+
+    half_book = size_book / 2
+    book_double = []
+    for elem is source_book:
+
+    pass_pages = []
+    pivot = 0
+    if half_book < wanted_page:
+        i = len(source_book - 1)
+        while wanted_page != pivot:
+            pivot = source_book[i]
+
+
+    print('half book {}'.format(half_book))
+    print('**************END***************')
+
     return True
+
 
 for _ in range(30):
     while not seek_page():
