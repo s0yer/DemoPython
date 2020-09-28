011f1d65b6d40b32646802201a9b70296f4bcacf
jadsonmarliere <jds.mdo@gmail.com>
2020-09-28 07:24:44 -0300

    Update breakingrecords.py

--- ongoing/breakingrecords.py
+++ ongoing/breakingrecords.py
@@ -1,7 +1,7 @@
 # Python 3.7
 
 from random import randint
-
+from aidfunctions import append_elements
 
 def breaking_records(n):
     size = n
@@ -9,18 +9,21 @@ def breaking_records(n):
     for elem in range(randint(21, 34)):
         source_list.append(randint(0, 100))
 
+    print('^^^^^^^^^ START ^^^^^^^^^^')
     print('The scores are: ' + str(source_list))
 
     # test quantity of records
     if 1 <= size <= pow(10, 3):
         print('size ok')
         if size < 2:
-            print('Only one element in the records, max and min = {}'.format(source_list[0]))
-            return True
+            ans = 'Only one element in the records, max and min = {}'.format(source_list[0])
+            print(ans)
+            return append_elements(source_list, ans)
         else:
             print('Continue process...')
     else:
         print('size -> [1, 10e3]')
+        print('****************** END *********************')
         return False
 
     # test magnitude of the record
@@ -53,12 +56,13 @@ def breaking_records(n):
     print('------------------------')
     print('Score min: : {}'.format(min_score))
     print('Min score beated {} times!'.format(times_beat_min))
-    print('------------------------')
-    print('end... ok process!')
+    print('#################### END #######################')
+
+    return append_elements(source_list, max_score, times_beat_max, min_score, times_beat_min)
 
 
-breaking_records(7)
-breaking_records(-1)
-breaking_records(100000)
+for i in range(randint(21, 34)):
+    breaking_records(randint(-1500, 1500))
+
 
 
