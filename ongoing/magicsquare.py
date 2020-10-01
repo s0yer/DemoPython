63b30ef4fcbec30006cdd46868555f9e411f27b4
jadsonmarliere <jds.mdo@gmail.com>
2020-10-01 00:56:29 -0300

    Update magicsquare.py

--- ongoing/magicsquare.py
+++ ongoing/magicsquare.py
@@ -19,19 +19,39 @@ def new_square():
 
 # sum of the rows, columns, or diagonal of length n need to be k(same value)
 def magic_square_evidence(source_square):
+    size = len(source_square)
     print(source_square)
     print(source_square[0][1])
     magic_number = sum(source_square[0])
     print(magic_number)
 
+    print('row test *********')
     # test of the rows
     for sub_list in source_square:
         if sum(sub_list) == magic_number:
-            print('Rows ok')
+            print('Row ok')
         else:
-            print('Its not a Magic Square')
+            print('Its not a Magic Square - row wrong sum')
             return False
 
+    print('column test *********')
+    # test of the rows
+    col = 0
+    row = 0
+    col_sum = 0
+    # column fixed
+    while col < len(size):
+        for i in range(size):
+            col_sum += source_square[i][col]
+        col += 1
+
+        if col_sum == magic_number:
+            print('Column ok.')
+        else:
+            print('Its not a Magic Square - column wrong sum')
+            return False
+
+    return True
 
 print(new_square())
 print('------------------------------')
