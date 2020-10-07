316c58914fd1b82bffdb34ea874c2c8054ebfcc7
jadsonmarliere <jds.mdo@gmail.com>
2020-10-07 13:59:35 -0300

    Create cifratransposicao.py

---
+++ ongoing/cifratransposicao.py
@@ -0,0 +1,34 @@
+# Python 3.7
+# UNICAMP - P_IA012A_2020S2 - Segurança em Comunicação de Dados
+""" Este e um projeto Academico ex.9
+> cifra de transposicao:
+- o texto a cifrar e escrito por colunas, ate n max de linhas
+- a mensagem e emitida via linhas """
+
+from random import randint
+
+text_p = 'jadsonmarlieredeoliveira'
+# k1 -> qtd linhas
+k1 = 0
+
+if k1 < 4:
+    k1 = 4
+else:
+    print('ok')
+
+size_p = len(text_p)
+# k2 -> qtd colunas
+k2 = size_p / 2
+
+
+matriz_obscura = []
+for elem in range(k1):
+
+
+print('Texto em claro: ')
+print(text_p)
+print('Chaves: ')
+print('k1 = {}'.format(k1))
+print('k2 = {}'.format(k2))
+
+
