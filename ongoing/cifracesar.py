3b0ca3cec8fb2bd815298ed2ac6ad0f8197480f4
jadsonmarliere <jds.mdo@gmail.com>
2020-10-08 19:26:37 -0300

    Update cifracesar.py

--- ongoing/cifracesar.py
+++ ongoing/cifracesar.py
@@ -1,6 +1,27 @@
 # Python 3.7
 
+from random import randint
+
 import string
 alfabeto = list(string.ascii_lowercase)
 print(alfabeto)
 
+fator = randint(5, len(alfabeto))
+texto_claro = 'jadsonamrlieredeoliveira'
+
+print('--------------------------------------------')
+print(fator)
+print(texto_claro)
+print('--------------------------------------------')
+
+def cifra_texto(fator, alfabeto, texto_claro):
+
+    texto_obscuro = []
+
+    
+    print(texto_obscuro)
+    print('--------------------------------------------')
+
+    return texto_obscuro
+
+cifra_texto(fator, alfabeto, texto_claro)
