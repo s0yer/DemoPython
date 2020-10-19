2749a3a5a68ad4f8836dc53d7a02feeff25e303a
jadsonmarliere <jds.mdo@gmail.com>
2020-10-19 11:23:50 -0300

    Create catandmouse.py

---
+++ ongoing/catandmouse.py
@@ -0,0 +1,17 @@
+# Python 3.7
+
+from random import randint
+
+def start_cat_mouse():
+
+    begin_limit = randint(21,34)
+    end_limit = randint(21,34)
+
+    blue_cat = randint(begin_limit, end_limit)
+    purple_cat = randint(begin_limit, end_limit)
+    dumb_mouse = randint(begin_limit, end_limit)
+
+
+def cat_and_mouse():
+
+    
