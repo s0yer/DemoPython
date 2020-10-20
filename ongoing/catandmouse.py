f912bd288a1e22147f29d07b48ac537f86453a84
jadsonmarliere <jds.mdo@gmail.com>
2020-10-20 07:21:53 -0300

    Update catandmouse.py

--- ongoing/catandmouse.py
+++ ongoing/catandmouse.py
@@ -1,17 +1,27 @@
 # Python 3.7
 
 from random import randint
+from math import fabs
 
 def start_cat_mouse():
 
-    begin_limit = randint(21,34)
-    end_limit = randint(21,34)
+    begin_limit = randint(21, 34)
+    end_limit = randint(21, 34)
 
     blue_cat = randint(begin_limit, end_limit)
     purple_cat = randint(begin_limit, end_limit)
     dumb_mouse = randint(begin_limit, end_limit)
 
+    return dumb_mouse, blue_cat, purple_cat
 
 def cat_and_mouse():
 
-    
+    dumb_mouse, blue_cat, purple_cat = start_cat_mouse()
+
+    blue_mouse_distance = fabs(dumb_mouse - blue_cat)
+    purple_cat_distance = fabs(dumb_mouse - purple_cat)
+
+    print(blue_mouse_distance)
+    print(purple_cat_distance)
+
+cat_and_mouse()
