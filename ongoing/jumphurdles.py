d4a7e15fd411df6af3aec7776f5a181052f7a015
jadsonmarliere <jds.mdo@gmail.com>
2020-10-26 08:58:18 -0300

    Update jumphurdles.py

--- ongoing/jumphurdles.py
+++ ongoing/jumphurdles.py
@@ -4,7 +4,6 @@ from random import randint
 
 def source_hurdle():
 
-    n = 4
     # k size of jump
     k = randint(3, 8)
     # list of the size of the objects
@@ -13,23 +12,29 @@ def source_hurdle():
     for i in range(randint(3, 8)):
         hurdle_list.append(randint(1, 12))
 
-    return n, k, hurdle_list
+    return k, hurdle_list
 
 def jump_hurdle():
 
-    n, k, hurdle_list = source_hurdle()
+    k, hurdle_list = source_hurdle()
 
     biggest_hurdle = max(hurdle_list)
 
-    if k > biggest_hurdle:
+    print('-------- Source ---------')
+    print('List of hurdles {}'.format(hurdle_list))
+    print('Jump size of the Code Dog = {}'.format(k))
+
+    if k >= biggest_hurdle:
         print('dont need potion, power jump = {} | Biggest hurdle = {}'.format(k, biggest_hurdle))
         return 0
     else:
-        print('ongoing')
-        return 1
+        potion = biggest_hurdle - k
+        print('The Code Dog need to take {} to complete the jump.'.format(potion))
+        return potion
 
 for i in range(40):
 
     jump_hurdle()
     print('-----------------------------')
 
+print('End process!!')
