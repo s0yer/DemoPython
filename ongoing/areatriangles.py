66c7dbccae775db31f9d17bf2ab62f5b3580b456
jadsonmarliere <jds.mdo@gmail.com>
2020-10-31 10:15:14 -0300

    Create areatriangles.py

---
+++ ongoing/areatriangles.py
@@ -0,0 +1,12 @@
+# Python 3.7
+
+from random import randint
+
+def source_data():
+
+    source_list = []
+    for i in range(55):
+        source_list.append(randint(5,89))
+
+    return source_list
+
