6e4851acfa391f56434222f3e43dbccdc5a1562f
jadsonmarliere <jds.mdo@gmail.com>
2020-11-03 22:13:07 -0300

    Update areatriangles.py

--- ongoing/areatriangles.py
+++ ongoing/areatriangles.py
@@ -4,9 +4,26 @@ from random import randint
 
 def source_data():
 
-    source_list = []
-    for i in range(55):
-        source_list.append(randint(5,89))
+    base_list = []
+    side_a_list = []
+    side_b_list = []
 
-    return source_list
+    for i in range(randint(34, 55)):
+        base_list.append(randint(5, 89))
+        side_a_list.append(randint(5, 89))
+        side_b_list.append(randint(5, 89))
+
+
+    return base_list, side_a_list, side_b_list
+
+def create_triangles():
+
+    base_list, side_a_list, side_b_list = source_data()
+
+    print(base_list)
+    print(side_a_list)
+    print(side_b_list)
+
+create_triangles()
+
 
